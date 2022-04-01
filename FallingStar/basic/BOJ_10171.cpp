962f83a5535ecdd59709d0ff6c00614972e8c117
최광호 <chlrhkdgh624@snu.ac.kr>
2022-04-01 17:46:28 +0900

    Upload: BOJ_10171

---
+++ FallingStar/basic/BOJ_10171.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cout << R"(\    /\)" << "\n";
+    cout << R"( )  ( '))" << "\n";
+    cout << R"((  /  ))" << "\n";
+    cout << R"( \(__)|)";
+    return 0;
+}
