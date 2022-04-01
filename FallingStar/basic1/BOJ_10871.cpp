252bdb64cc217d91f94aff11080a642c5a300fdf
최광호 <chlrhkdgh624@snu.ac.kr>
2022-04-01 17:11:22 +0900

    Upload: BOJ_10871

---
+++ FallingStar/basic1/BOJ_10871.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int N, X;
+    cin >> N >> X;
+    int arr[N];
+    for (int i=0; i < N; i++) {
+        cin >> arr[i];
+    }
+    for (int j=0; j<N; j++) {
+        if (arr[j] < X) {
+            cout << arr[j] << " ";
+        }
+    }
+    return 0;
+}
