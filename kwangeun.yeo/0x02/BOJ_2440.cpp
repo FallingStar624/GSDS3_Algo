a6d80704c5f469065c727d955ec91303b6f9feca
kwangeun <yeokwangeun@gmail.com>
2022-04-02 11:05:24 +0900

    0x02 solutions for drawing stars

---
+++ kwangeun.yeo/0x02/BOJ_2440.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+//    freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 0; j < N - i + 1; ++j) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
