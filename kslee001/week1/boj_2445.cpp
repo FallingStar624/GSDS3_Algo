19c76f1ac5bfd03077203ec3bb174222b901a465
kslee001 <ksnannaya@gmail.com>
2022-04-05 20:24:10 +0900

    week1

---
+++ kslee001/week1/boj_2445.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char s = '*'; char b = ' ';
+    int c;
+    int n; cin>>n;
+
+    for(int i=0; i<n; ++i){
+        c = n-i;
+        for(int j=0; j<=i; ++j) cout << s;
+        for(int j=0; j<c-1 ; ++j) cout << b;
+        for(int j=0; j<c-1 ; ++j) cout << b;
+        for(int j=0; j<=i; ++j) cout << s;
+        cout << "\n";
+    }
+    for(int i=0; i<n-1; i++){
+        c = n-i;
+        for(int j=0; j<c-1; ++j) cout << s;
+        for(int j=0; j<=i ; ++j) cout << b;
+        for(int j=0; j<=i ; ++j) cout << b;
+        for(int j=0; j<c-1; ++j) cout << s;
+        if (i!= n-2) cout << "\n";
+    }
+    
+
+}
