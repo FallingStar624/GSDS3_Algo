8ecac951be36828f2332fd451c2e52403220ae9f
최광호 <chlrhkdgh624@snu.ac.kr>
2022-05-04 13:22:33 +0900

    Upload: BOJ_10845

---
+++ FallingStar/0x06/BOJ_10845.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    int n, i, tmp; cin>>n;
+    queue<int> Q;
+    string command;
+    for (i=0; i<n; i++) {
+        cin >> command;
+        if (command == "push") {
+            cin >> tmp;
+            Q.push(tmp);
+        }  else if (command == "pop") {
+            if (Q.empty()) {
+                cout << -1 << "\n";
+            } else {
+                tmp = Q.front();
+                Q.pop();
+                cout << tmp << "\n";
+            }
+        } else if (command == "size") {
+            cout << Q.size() << "\n";
+        } else if (command == "empty") {
+            cout << int(Q.empty()) << "\n";
+        } else if (command == "front") {
+            if (Q.empty()) {
+                cout << -1 << "\n";
+            } else {
+                cout << Q.front() << "\n";
+            }
+        } else {
+            if (Q.empty()) {
+                cout << -1 << "\n";
+            } else {
+                cout << Q.back() << "\n";
+            }
+        }
+    }
+    return 0;
+}
