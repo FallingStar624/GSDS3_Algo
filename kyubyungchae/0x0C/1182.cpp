5478fbe2dfcf5d4e4cfca9f0abac1d032259d1fd
Kyubyung Chae <kyubyungchae@Kyubyungui-MacBookAir.local>
2022-07-01 16:08:48 +0900

    delete hello.txt and 0x0C upload

---
+++ kyubyungchae/0x0C/1182.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+
+    return 0;
+}
---
+++ kyubyungchae/0x0C/15651.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int n,m;
+int arr[10];
+bool isused[10];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    
+    return 0;
+}
