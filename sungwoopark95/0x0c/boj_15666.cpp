29725d75bd2b9c0b3e770af21608ab15bfcbd9bf
sungwoopark95 <swpark149531@gmail.com>
2022-07-05 17:24:19 +0900

    solved all N and M problems

---
+++ sungwoopark95/0x0c/boj_15666.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+using namespace std;
+
+int N, M;
+int arr[10];
+int seq[10];
+set<vector<int>> s;
+
+void func(int cur) {
+    if (cur == M) {
+        vector<int> vec;
+        for (int i=0;i<M;i++) { vec.push_back(arr[i]); }
+        s.insert(vec);
+        return;
+    }
+    
+    for (int i=0;i<N;i++) {
+        if (cur>=1 && arr[cur-1] > seq[i]) { continue; }
+        arr[cur] = seq[i];
+        func(cur+1);
+    }
+}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    
+    cin >> N >> M;
+    for (int i=0;i<N;i++) {
+        cin >> seq[i];
+    }
+    sort(seq, seq+N);
+    
+    func(0);
+    for (auto itr=s.begin();itr!=s.end();itr++) {
+        vector<int> item = *itr;
+        for (int i=0;i<(int)item.size();i++) {
+            cout << item[i] << " ";
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
