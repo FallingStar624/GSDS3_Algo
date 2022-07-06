850c6d79081453761de03c4b12c091bd704a8045
최광호 <chlrhkdgh624@snu.ac.kr>
2022-07-06 14:56:52 +0900

    Upload: BOJ_15656

---
+++ FallingStar/0x0C/BOJ_15656.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int n, m;
+vector<int> arr;
+string stop;
+
+void back_track(string visit) {
+    if (visit.length()==m) {
+        for (char c : visit) {
+            cout << arr[c-'1'] << " ";
+        }
+        if (visit!=stop) {
+            cout << "\n";
+        }
+        return;
+    }
+    for (int j=1; j<n+1; ++j) {
+        bool check = true;
+//        for (char c : visit) {
+//            if (c-'0' == j){
+//                check=false;
+//            }
+//        }
+        if (check) {
+            back_track(visit+ to_string(j));
+        }
+    }
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n >> m;
+    for (int i=0; i<n; ++i) {
+        int tmp;
+        cin >> tmp;
+        arr.push_back(tmp);
+    }
+    sort(arr.begin(), arr.end());
+    string start;
+    for (int i=n; i>n-m; --i) {
+        stop += to_string(n);
+    }
+    back_track(start);
+    return 0;
+}
