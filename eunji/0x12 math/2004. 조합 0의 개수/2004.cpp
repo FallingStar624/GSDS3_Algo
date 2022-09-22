199ce49a840e70fbe78f82ed379a0a5b78544ea6
eungi-aiza <kuman5262a@gmail.com>
2022-09-22 16:03:37 +0900

    finished math

--- eunji/0x12 math/2004. 조합 0의 개수/2004.cpp
+++ eunji/0x12 math/2004. 조합 0의 개수/2004.cpp
@@ -10,60 +10,35 @@ using namespace std;
 long long n, m;
 long long answer = 0;
 
-int counttwo(long long n){
+int countfive(long long n){
     int ans = 0;
-    for (long long i=1; i<=n; i++){
-        int count = 0;
-        while (1){
-            if (i % 2 == 0) count ++;
-            else break;
-        }
-        ans += count;
-    }   
+    while (n >= 5){
+        ans += n/5;
+        n /= 5;
+    }
     return ans;
 }
 
-int countfive(long long n){
+int counttwo(long long n){
     int ans = 0;
-    for (long long i=1; i<=n; i++){
-        int count = 0;
-        while (1){
-            if (i % 5 == 0) count ++;
-            else break;
-        }
-        ans += count;
-    }   
+    while (n >= 2){
+        ans += n/2;
+        n /= 2;
+    }
     return ans;
 }
+
 
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n >> m;
     // cout << n;
-    cout << counttwo(25);
+    // cout << counttwo(25);
 
-    // int answer = min(counttwo(n)-counttwo(n-m)-counttwo(m), countfive(n)-countfive(n-m)-countfive(m));
+    int answer = min(counttwo(n)-counttwo(n-m)-counttwo(m), countfive(n)-countfive(n-m)-countfive(m));
 
-    // cout << answer;
+    cout << answer;
     return 0;
 }
 
-
-
-
-// int main(void){
-//     ios::sync_with_stdio(0);
-//     cin.tie(0);
-//     cin >> n >> m;
-
-//     product = LCM(n, m);
-//     while (product % 10 == 0){
-//         product = product / 10;
-//         answer ++;
-//     }
-
-//     cout << answer;
-
-//     return 0;
-// }
