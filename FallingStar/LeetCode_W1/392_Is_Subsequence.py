e03c3779afffa8f43dd7922ee02ff10b4be5a684
최광호 <chlrhkdgh624@snu.ac.kr>
2023-01-20 01:10:41 +0900

    Upload: leetcode 392

---
+++ FallingStar/LeetCode_W1/392_Is_Subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        if i == len(s):
+            return True
+        else:
+            return False
+
+
+
+
+
