ff40cd8dd91076c193ec1e6c2f636d049ffed02f
최광호 <chlrhkdgh624@snu.ac.kr>
2023-02-13 14:33:37 +0900

    Update: leetcode235

--- FallingStar/LeetCode_W4/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ FallingStar/LeetCode_W4/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -1,4 +1,6 @@
-low = min(p.val, q.val)
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        low = min(p.val, q.val)
         high = max(p.val, q.val)
         while True:
             if root.val < low:
