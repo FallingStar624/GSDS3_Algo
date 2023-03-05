172a017ff4b182d3043fd11e420dbec19b68e8b7
eungi-aiza <kuman5262a@gmail.com>
2023-03-05 18:22:46 +0900

    <update> 0305

--- eunji/leetcode/230306/797_All_Paths_from_Source_to_Target.py
+++ eunji/leetcode/230306/797_All_Paths_from_Source_to_Target.py
@@ -1,15 +1,25 @@
 from typing import List
 
 class Solution:
-    def dfs(self, graph, visited, paths, curr):
-        paths.append([curr])
+    def __init__(self):
+        self.paths = []
+
+    def dfs(self, graph, path, curr, target):
+        if curr == target:
+            self.paths.append(path)
+            return
+
         for child in graph[curr]:
-            if child not in visited:
-                visited.append(child)
-                self.dfs(graph, visited, paths, child)
-        return ans 
+            self.dfs(graph, path + [child], child, target)
     
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        visited = []
-        paths = []
-        return self.dfs(self, graph, visited, paths, 0)
+        path = [0]
+        target = len(graph)-1
+        self.dfs(graph, path, 0, target)
+        return self.paths
+    
+if __name__ == '__main__':
+    S = Solution()
+    graph = [[2],[],[1]]
+    A = S.allPathsSourceTarget(graph)
+    print(A)
