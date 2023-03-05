734e24b0dcc5ec67522817ea3ea233a9267fa70f
eungi-aiza <kuman5262a@gmail.com>
2023-03-05 22:36:44 +0900

    <complete> 0305

--- eunji/leetcode/230306/394_Decode_String.py
+++ eunji/leetcode/230306/394_Decode_String.py
@@ -1,33 +1,40 @@
-class Solution:
-    def decodeOne(self, one):
-        num = int(one[0])
-        repeat = one[2:-2]
-        ans = num * repeat
-        return ans
-            
-            
+class Solution:            
     def decodeString(self, s: str) -> str:
+        stack = []
         start = 0
-        end = len(s)
-        ans = []
-        ans2 = []
-        while (end > start):
+        nums = []
+        while (len(s) > start):
+            if s[start].isdigit() and len(s) == 1:
+                return ""
             if s[start].isdigit():
-              num = []
-              while s[start].isdigit():
-                  num.append(s[start])
-                  start += 1
-            elif s[start] == "[" & s[end] == "]":
-               self.decodeOne(s, start, end, num)
+                num = []
+                while s[start].isdigit():
+                    num.append(s[start])
+                    start += 1
+                nums.append(int("".join(num)))
+                # print(stack)
+                # print(num)
+            elif s[start] == "]":
+                temp = []
+                while(stack[-1] != "["):
+                    pop = stack.pop()
+                    temp.insert(0, pop)
+                stack.pop() # "["
+                for i in range(nums.pop()):
+                    stack.extend(temp)
+                start += 1
+            else:
+                stack.append(s[start])
+                start += 1
 
-            elif s[start] != "[" & s[end] == "]":
-               end -= 1
-            elif s[end].isdigit() == False:
-               ans2.append(s[end])
-               end -= 1
-               
-            if s[start].isdigit() == False:
-              ans.append(s[start])
-              start += 1
-            self.decodeOne(s, start, end)
+        return "".join(stack)          
                 
+  
+if __name__ == '__main__':
+    S = Solution()
+    graph = s = "3"
+    # k = 3
+    A = S.decodeString(graph)
+    print(A)
+            
+            
--- eunji/leetcode/230306/692_Top_K_Frequent_Words.py
+++ eunji/leetcode/230306/692_Top_K_Frequent_Words.py
@@ -1,5 +1,28 @@
 from typing import List
+from collections import Counter
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        
+        ans = []
+        if words == None:
+           return ans
+        wordsNum = Counter(words)
+        wordsNum_K = dict()
+        for w, num in wordsNum.items():
+            if num in wordsNum_K:
+              wordsNum_K[num].extend([w])
+            else:
+              wordsNum_K[num] = [w]
+        for i in sorted(wordsNum_K, reverse=-1):
+           ans.extend(sorted(wordsNum_K[i]))
+        return ans[:k]
+
+                
+if __name__ == '__main__':
+    S = Solution()
+    graph = ["i","love","leetcode","i","love","coding"]
+    k = 3
+    A = S.topKFrequent(graph,k)
+    print(A)
+            
+            
