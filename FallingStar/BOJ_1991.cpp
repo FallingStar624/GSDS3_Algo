c53c86efb27b6953431185c2300103c9ae5d531e
FallingStar624 <chlrhkdgh624@snu.ac.kr>
2023-08-17 02:59:14 +0900

    Upload: BOJ_1991

---
+++ FallingStar/BOJ_1991.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+int n;
+char p, l, r;
+
+unordered_map<char, char> leftChild;
+unordered_map<char, char> rightChild;
+
+void preorder(char parent) {
+    if (parent == '.') return;
+    cout << parent;
+    preorder(leftChild[parent]);
+    preorder(rightChild[parent]);
+}
+
+void postorder(char parent) {
+    if (parent == '.') return;
+    postorder(leftChild[parent]);
+    cout << parent;
+    postorder(rightChild[parent]);
+}
+
+void inorder(char parent) {
+    if (parent == '.') return;
+    inorder(leftChild[parent]);
+    inorder(rightChild[parent]);
+    cout << parent;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        cin >> p >> l >> r;
+        leftChild[p] = l;
+        rightChild[p] = r;
+    }
+
+    preorder('A'); cout << endl;
+    postorder('A'); cout << endl;
+    inorder('A');
+
+    return 0;
+}
+
